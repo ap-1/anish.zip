ba65d586362e4edd4e3958b35a2e481305a83e24
Anish Pallati <anishp@duck.com>
2023-09-30 16:03:15 -0400

    feat: Set up Tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,23 @@
+import { fontFamily } from "tailwindcss/defaultTheme";
 import { type Config } from "tailwindcss";
 
 export default {
+	darkMode: "class",
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Satoshi", ...fontFamily.sans],
+			},
+			colors: {
+				text: "var(--text)",
+				background: "var(--background)",
+				border: "var(--border)",
+				primary: "var(--primary)",
+				secondary: "var(--secondary)",
+				accent: "var(--accent)",
+			},
+		},
 	},
 	plugins: [],
 } satisfies Config;
